60c4e9db8c2967cc5b61fd9a5cfa7f92cd0ba31b
lakmal05 <lakmaljayawardhana59@gmail.com>
2023-02-23 16:37:30 +0530

    rend entity

---
+++ Back-End/src/main/java/lk/ijse/spring/entity/Rent.java
@@ -0,0 +1,51 @@
+package lk.ijse.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
+public class Rent {
+
+     @Id
+    private String rentId;
+    private String rentDate;
+    private String pickUpDate;
+    private String returnDate;
+    private String pickUpVenue;
+    private String returnVenue;
+    private String lossDamageWaiver;
+    private String bankSlip;
+    private String status;
+
+
+    @Id
+    private String registrationNO;
+    @Id
+    private String customerId;
+
+
+
+    @ManyToOne(cascade ={CascadeType.REFRESH,CascadeType.DETACH})
+    @JoinColumn(name ="licenceNo",referencedColumnName = "licenceNo",nullable = false)
+    private Driver licenceNo;
+
+
+    @ManyToOne
+    @JoinColumn(name = "registrationNO",referencedColumnName = "registrationNO",insertable =false,updatable = false)
+    private Car car;
+
+    @ManyToOne
+    @JoinColumn(name = "customerId",referencedColumnName = "customerId",insertable = false,updatable = false)
+    private Customer customer;
+
+
+
+}
---
+++ Back-End/src/main/java/lk/ijse/spring/entity/Rent_PK.java
@@ -0,0 +1,20 @@
+package lk.ijse.spring.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Rent_PK implements Serializable {
+
+    private String registrationNO;
+    private String customerId;
+
+
+}
